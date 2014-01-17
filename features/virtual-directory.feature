f6d86a133d5f939d2457d312ae60974ba75e14a2
Jeremy Green <jeremy@octolabs.com>
2014-01-17 15:10:19 -0600

    Be more explicit about virtual directories

--- features/virtual-directory.feature
+++ features/virtual-directory.feature
@@ -1,5 +1,5 @@
 Feature: Virtual directories
   Given a fixture app "alias-app"
-  When I go to "/posts/2010/01/01/virtual-test"
+  When I go to "/posts/2010/01/01/virtual-test/"
   Then I should see "You are being redirected"
    
--- lib/middleman-alias/extension.rb
+++ lib/middleman-alias/extension.rb
@@ -10,7 +10,7 @@ module Middleman
       resources.each do |resource|
         if resource.data["alias"]
           alias_url = resource.data["alias"]
-          alias_url += "/index.html" unless alias_url.match(/\.html$/)
+          alias_url += "/index.html" if alias_url.match(/\/$/)
           Sitemap::Resource.new(@app.sitemap, alias_url).tap do |p|
             p.proxy_to("alias.html")
             p.add_metadata locals: {
