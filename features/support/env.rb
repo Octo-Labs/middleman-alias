dcc71eef6d4a4db7e9e6a7c45c6fe9b46778b1d5
Jeremy Green <jeremy@octolabs.com>
2014-02-06 01:00:17 -0600

    Add coveralls

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,3 +5,6 @@ PROJECT_ROOT_PATH = File.dirname(File.dirname(File.dirname(__FILE__)))
 require "middleman-core"
 require "middleman-core/step_definitions"
 require File.join(PROJECT_ROOT_PATH, 'lib', 'middleman-alias')
+
+require 'coveralls'
+Coveralls.wear!
